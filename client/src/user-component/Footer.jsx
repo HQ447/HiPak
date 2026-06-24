4152d5833ef444407fbb62fca335347c387efb9c
Hammad Ahmad <hamadqur447@gmail.com>
2026-06-24 08:47:43 +0500

    working on Home page layout

--- client/src/user-component/Footer.jsx
+++ client/src/user-component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer">
+        <footer className="footer border border-gray-300 py-6 px-5 sm:px-10 md:px-20">
             <p>&copy; 2026 HiPak. All rights reserved.</p>
         </footer>
     );
